4273415a75f6c160c4ecf3182cd9e67a8f1637b1
zoodix <132278000+Alihan2011@users.noreply.github.com>
2024-04-28 17:08:39 +0500

    Update my_memory_card.py

--- my_memory_card.py
+++ my_memory_card.py
@@ -7,7 +7,7 @@ win_height = 700
 window = display.set_mode((win_width, win_height))
 display.set_caption('')
 background = (200,200,255)
-background = transform.scale(image.load('i.webp'),(win_width, win_height))
+background = transform.scale(image.load('y.jpg'),(win_width, win_height))
 font.init()
 font1 = font.SysFont('Arial',100)
 font2 = font.SysFont('Arial',50)
@@ -15,7 +15,7 @@ mixer.init()
 
 mixer.music.load('background.mp3')
 mixer.music.play()
-
+life = 100
 
 
 game = True
@@ -46,6 +46,10 @@ class Player(GameSprite):
     def fire(self):
         bullet = Bullet('kit.png',self.rect.centerx,self.rect.top,20,15,20)
         bullets.add(bullet)
+    def check_collision(self, player_rect, obstacle_rect):
+        if player_rect.left < obstacle_rect.right and player_rect.right > obstacle_rect.left and player_rect.top < obstacle_rect.bottom and player_rect.bottom > obstacle_rect.top:
+           return True
+        return False
 
 class Wall(sprite.Sprite):
     def __init__(self,color_1,color_2,color_3,x,y,width,height):
@@ -58,9 +62,10 @@ class Wall(sprite.Sprite):
     def draw_wall(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 class Enemy(GameSprite):
+    obstacle = Rect(200, 200, 100, 100)
     def __init__(self,enemy_image,x,y,w,h):
         self.image = transform.scale(image.load(enemy_image),(w,h))
-        self.image = Surface((w,h))
+        #self.image = Surface((w,h))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -80,16 +85,63 @@ class Bullet(GameSprite):
         self.rect.y -=self.speed
         if self.rect.y<0:
             self.kill()#-----------как удалить с экрана что нибудь
-enemy1 = Enemy('elf.png',700,600,50,50,)
-enemy2 = Enemy('elf.png',100,600,50,50,)
-player = Player('heart.png',300,440,10,50,50,100)
+enemy1 = Enemy('elf.png',700,400,75,75,)
+enemy2 = Enemy('elf.png',100,600,75,75,)
+enemy3 = Enemy('elf.png',850,600,75,75,)
+enemy4 = Enemy('elf.png',200,600,75,75,)
+enemy5 = Enemy('elf2.png',900,550,115,130,)
+player = Player('ii.png',300,440,10,50,50,100)
+x = 200
+y = 600
+
+player_x = player.rect.x
+player_y = player.rect.y
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    player_x = player.rect.x
+    player_y = player.rect.y
+    
     if not finish:
-        #if self.rect.x == 595:
-            #quest1 = fon1.render('Вы уверены идти дальше?да-1,нет-2')
+        if check_collision(player_rect, obstacle_rect):
+            player_rect.x = previous_player_x
+            player_rect.y = previous_player_y
+
+        
+        if player_x == x and player_y == y:
+            quest1=font1.render(f"1-спросить где мы 2-спросить как они попали сюда 3- выйти", True, (255, 0, 0))
+            window.blit(quest1, (750, 450))
+            if keys[K_1]:
+                answ1 = font1.render(f'я - Джейкоб и мы сейчас находимся на тропинке к замку, если ты туда идешь то иди на право',True,(255,0,0))
+                window.blit(answ1,(750,450))
+                quest1.kill()
+            if keys[K_2]:
+                answ2 = font1.render(f'Меня зовут Джейкоб, и я - эльф. Сегодня вечером я стою с друзьями возле замка на зимней тропинке.
+                 Мы собрались здесь, чтобы отпраздновать нашу победу в последней битве против орков.
+                Вокруг нас воцарилась тишина, только слабый ветер шепчет свои таинственные истории. 
+                Замок выглядит величественно, освещенный лунным светом.
+                 Мы смотрим на него с восхищением, чувствуя себя настоящими героями.',True,(255,0,0))
+                window.blit(answ2,(750,450))
+                quest1.kill()
+            elif keys[K_3]:
+                quest1.kill()
+                answ1.kill()
+                answ2.kill()
+        if life >=70:
+            life_text_color = (0,255,0)    
+        if life >=20 and life<70:
+            life_text_color = (250,200,50)
+        if life <=0 or life<20:
+            life_text_color = (255,0,0)     
+        if life <=0:
+            finish = True
+            window.blit(lose,(250,250))
+        life_text = font1.render(f'Здоровье:{life}', True ,life_text_color)
+        window.blit(life_text,(10,90))
+        #if self.rect.x > 595:
+            #tropinka = fon1.render('Вы уверены идти дальше?да-9,нет-0')
             #window.blit(quest1, (200,200))
 
         window.blit(background,(0,0))
@@ -99,7 +151,15 @@ while game:
         enemy1.update()
         enemy2.reset()
         enemy2.update()
-
-
+        enemy3.reset()
+        enemy3.update()
+        enemy4.reset()
+        enemy4.update()
+        enemy5.reset()
+        enemy5.update()
+    else:
+        finish=False
+    
+    
     time.delay(20)
     display.update()
